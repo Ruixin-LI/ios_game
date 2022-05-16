bf543bf50595b2c06dca2c34ea7ef0a23adc9a1a
Li <ruixinli0520@gmail.com>
2022-05-16 15:26:44 +0800

    Initial Commit

---
+++ memorize/memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  memorizeApp.swift
+//  memorize
+//
+//  Created by Li Ruixin on 16/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
